9754b0e9245e0035174c6f31c430abdb90648655
Elena <elena.lomachenko.el@gmail.com>
2018-06-24 21:05:48 +0700

    MyFirstTest

---
+++ new_gradle/src/test/java/Tests/MyFirstTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest extends OptionsTests {
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.id("tsf")).click();
+        driver.findElement(By.name("btnK")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+}
