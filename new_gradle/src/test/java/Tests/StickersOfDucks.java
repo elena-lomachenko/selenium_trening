44222a96b5931aec5e27816514b8fa804f83248a
Elena <elena.lomachenko.el@gmail.com>
2018-07-01 19:03:37 +0700

    Тест StickersOfDucks
    Задание 8

--- new_gradle/src/test/java/Tests/StickersOfDucks.java
+++ new_gradle/src/test/java/Tests/StickersOfDucks.java
@@ -1,8 +1,11 @@
 package Tests;
-
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementValue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -11,8 +14,21 @@ public class StickersOfDucks extends OptionsTests {
 
     @Test
     public void stickersOfDucks() {
+        driver.navigate().to( "http://localhost/litecart/" );
+        List<WebElement> products = driver.findElements( By.cssSelector( "li.product" ) );
 
-        driver.navigate().to( "http://localhost/litecart/admin/" );
+        for(WebElement product : products) {
+            assert isDucksOneSticker(product) : "Product has more than one sticker or no stickers";
+        }
+    }
 
+    private boolean isDucksOneSticker(WebElement product) {
+        return product.findElements(By.cssSelector("div.sticker")).size() == 1;
     }
 }
+
+
+
+
+
+
