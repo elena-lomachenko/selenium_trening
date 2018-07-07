2dc83f58712cfc44e546a5f7b9c79cac25fcdd92
Elena <elena.lomachenko.el@gmail.com>
2018-07-07 18:38:07 +0700

    Тест Multilinks2

--- new_gradle/src/test/java/Tests/MultiLinks2.java
+++ new_gradle/src/test/java/Tests/MultiLinks2.java
@@ -11,9 +11,6 @@ public class MultiLinks2 extends OptionsTests {
 
     @Test
     public void multiLinks() {
-        //*************************
-        //Заходим на сайт и авторизуемся
-        //*************************
 
         driver.navigate().to( "http://localhost/litecart/admin/" );
         driver.findElement( By.name( "username" ) ).sendKeys( "admin" );
@@ -22,22 +19,22 @@ public class MultiLinks2 extends OptionsTests {
         wait.until( ExpectedConditions.visibilityOf( driver.findElement( By.xpath( "//div[@id='notices']" ) ) ) );
 
 
-        List<WebElement> menuItems = driver.findElements( By.cssSelector( "#box-apps-menu > li[id='app-'] > a" ) );
-        int top_level_links_amount = menuItems.size();
+        List<WebElement> menu = driver.findElements( By.cssSelector( "#box-apps-menu > li[id='app-'] > a" ) );
+        int top_level_links_amount = menu.size();
 
         for (int i = 0; i < top_level_links_amount; i++) {
-            menuItems = driver.findElements( By.cssSelector( "#box-apps-menu > li[id='app-'] > a" ) );
-            menuItems.get( i ).click();
+            menu = driver.findElements( By.cssSelector( "#box-apps-menu > li[id='app-'] > a" ) );
+            menu.get( i ).click();
 
             try {
                 List<WebElement> sub_menu = driver.findElements( By.className( "docs" ) );
-                List<WebElement> sub_menu_items = sub_menu.get( 0 ).findElements( By.cssSelector( "li > a" ) );
-                int sub_menu_size = sub_menu_items.size();
+                List<WebElement> puncts = sub_menu.get( 0 ).findElements( By.cssSelector( "li > a" ) );
+                int sub_menu_size = puncts.size();
 
                 for (int j = 0; j < sub_menu_size; j++) {
                     sub_menu = driver.findElements( By.className( "docs" ) );
-                    sub_menu_items = sub_menu.get( 0 ).findElements( By.cssSelector( "li > a" ) );
-                    sub_menu_items.get( j ).click();
+                    puncts = sub_menu.get( 0 ).findElements( By.cssSelector( "li > a" ) );
+                    puncts.get( j ).click();
                     WebElement content = driver.findElement( By.id( "content" ) );
                     WebElement content_h1 = content.findElement( By.tagName( "h1" ) );
                     assert content_h1.isDisplayed();
